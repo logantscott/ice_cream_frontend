ba8257feb54b99fba905c2322da1e6a409f18fed
Logan Scott <logantscott@gmail.com>
2020-02-20 18:00:26 -0800

    initial

---
+++ src/ice-cream.js
@@ -0,0 +1,200 @@
+const flavors = [
+    {
+        id: 1,
+        flavor: 'Vanilla Bean',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 2,
+        flavor: 'Dutch Chocolate',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 3,
+        flavor: 'Sweet Coconut',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: true,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 4,
+        flavor: 'Mint Chocolate Chip',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 5,
+        flavor: 'Strawberry',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 6,
+        flavor: 'Mocha Almond Fudge',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 7,
+        flavor: 'Salted Caramel',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 8,
+        flavor: 'Mango Sherbert',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: true,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 9,
+        flavor: 'Dulce De Leche',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 10,
+        flavor: 'Chocolate Chip Cookie Dough',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 11,
+        flavor: 'Cookies \'n\' Cream',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 12,
+        flavor: 'Butter Pecan',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 13,
+        flavor: 'Black Walnut',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 14,
+        flavor: 'Birthday Cake',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 15,
+        flavor: 'Cherry',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: true,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 16,
+        flavor: 'Espresso Chip',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 17,
+        flavor: 'Green Tea',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: true,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 18,
+        flavor: 'Rocky Road',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 19,
+        flavor: 'Peanut Butter Chocolate',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 20,
+        flavor: 'Pistachio',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 21,
+        flavor: 'Lemon Sherbert',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: true,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    },
+    {
+        id: 22,
+        flavor: 'Original Tart',
+        img_url: VARCHAR(256),
+        type: VARCHAR(256),
+        vegan: false,
+        will_licks: INTEGER,
+        logan_licks: INTEGER
+    }
+]
